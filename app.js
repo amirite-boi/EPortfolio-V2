d4758140a3d74259cb157b0bd2fffcf8d703d471
Amiri <a.m.scrutchin@gmail.com>
2025-07-26 00:52:30 -0500

    toggle modal, dark mode, bg animation, responsiveness

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 // template_72kfvk9
 //service_f1ad2xi
 //TkuBd0reV6YN7nVRl
+let isDark = false;
+let isModalOpen = false;
+const scaleFactor = 1 / 20;
+
+function toggleContrast() {
+    console.log("works")
+    if (isDark) {
+        isDark = !isDark;
+        return document.body.classList.remove("contrast__on")
+    }
+    isDark = !isDark;
+    document.body.classList += " contrast__on";
+}
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i=0; i<shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const oddInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x *oddInt}px, ${y * oddInt}px)`;
+    }
+}
 
 function contact(event) {
     event.preventDefault()
@@ -23,4 +48,14 @@ function contact(event) {
                 "The email service is temporarily unavailable. Please contact me directly via ams.artstudios@gmail.com"
             )
         })
+}
+
+function toggleModal() {
+    console.log("works")
+    if (isModalOpen) {
+        isModalOpen = !isModalOpen;
+        return document.body.classList.remove("modal__open")   
+    }
+    isModalOpen = !isModalOpen;
+    document.body.classList += " modal__open"   
 }
